6b74d4960c31bb3c82ef14b2d78a8b9578c24f0d
syuyitung <samyuyitung@gmail.com>
2016-07-23 12:17:02 -0400

    more stuff

--- src/JebBushIsAMess/AppMain.java
+++ src/JebBushIsAMess/AppMain.java
@@ -26,9 +26,13 @@ public class AppMain implements KeyListener, ActionListener {
 
 	ImageIcon startScreen = new ImageIcon("start.jpg");
 	ImageIcon background = new ImageIcon("background.jpg");
+	ImageIcon gameOver = new ImageIcon("start.jpg");
 
 	List<Enemy> enemies = new ArrayList<>();
 	JPanel jpane = new JPanel();
+
+	TTimer timer;
+	Counter t;
 
 	public AppMain() {
 		frame.addKeyListener(this);
@@ -41,21 +45,94 @@ public class AppMain implements KeyListener, ActionListener {
 
 	class Drawing extends JComponent {
 		public void paint(Graphics g) {
-			if (gameState == 1){
-				startScreen();
+			if (gameState == 1) {
+				drawStartScreen();
 				g.drawImage(startScreen.getImage(), 0, 0, this);
-			}else if(gameState == 2){
+			} else if (gameState == 2) {
 				g.drawImage(background.getImage(), 0, 0, this);
-				for(Enemy e : enemies)
+				for (Enemy e : enemies)
 					e.drawEnemy(g);
-					
-			}	
+			}
+				else if (gameState == 3) {
+					g.drawImage(gameOver.getImage(), 0, 0, this);
+			}
 		}
-		
+
 	}
 
-	void startScreen() {
-		jpane.setLayout(new GridLayout(1,1));
+	public void start() {
+		timer = new TTimer(1000, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				synchronized (timer) {
+//					winConditions();
+//					block.fall();
+//					if (block.idk)
+//						block.idk = timer.fast = false;
+				}
+
+				draw.repaint();
+			}
+		});
+		timer.start(); // pauses immediately
+		t = new Counter();
+		t.start();
+
+	}
+
+	// Timer Class
+	// handles the falling of the block timer and in game mechanic thread
+	public class TTimer extends Thread {
+		long delay;
+		boolean pause = false;
+		boolean fast = false;
+		ActionListener a;
+
+		public TTimer(long adelay, ActionListener aa)
+	      { 
+	         delay = adelay;
+	         a = aa;
+	      }
+
+		public void run() {
+			int i = 0;
+			while (i < 100) {
+				i += 1;
+				try {
+				} catch (Exception e) {
+				}
+				if (!pause)
+					a.actionPerformed(null);
+
+			}
+			drawGameOver();
+		}
+	}
+
+	// Counter class (in game time)
+	// used to track the time ingame
+	public class Counter extends Thread {
+		double seconds = 0;
+		int minutes = 0;
+
+		public void run() {
+			try {
+				while (true) {
+					sleep(100);
+					if (timer != null && !timer.pause)
+						seconds += 0.1;
+					if (seconds > 59) {
+						seconds = 0;
+						minutes++;
+
+					}
+				}
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	void drawStartScreen() {
+		jpane.setLayout(new GridLayout(1, 1));
 		startButton.setOpaque(true);
 		startButton.setContentAreaFilled(false);
 		startButton.setBorderPainted(false);
@@ -65,13 +142,19 @@ public class AppMain implements KeyListener, ActionListener {
 		frame.revalidate();
 		frame.repaint();
 	}
-	void drawGameBoard(){
+
+	void drawGameBoard() {
 		frame.remove(jpane);
+	      frame.requestFocus();
 	}
 
-	void makeAnEnemy(){
+	void drawGameOver(){
+		
+	}
+	void makeAnEnemy() {
 		enemies.add(new Enemy(100, 1));
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -99,13 +182,13 @@ public class AppMain implements KeyListener, ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == startButton){
-			gameState = 2; 
+		if (e.getSource() == startButton) {
+			gameState = 2;
 			drawGameBoard();
 			makeAnEnemy();
 		}
 		frame.revalidate();
-	      frame.repaint();
+		frame.repaint();
 
 	}
 }
