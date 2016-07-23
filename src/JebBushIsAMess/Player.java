fa018e0e152e738dc4865388f0f038d25023042e
Andrew Lee <andrewjplee586@gmail.com>
2016-07-23 12:18:20 -0400

    added player

---
+++ src/JebBushIsAMess/Player.java
@@ -0,0 +1,37 @@
+package JebBushIsAMess;
+
+import java.awt.Color;
+import java.awt.Graphics; 
+//import java.awt.Color; 
+
+public class Player { 
+	// variables 
+	private int x_pos; 
+	private int y_pos; 
+	public boolean jumping;
+	private int width;
+	private int height;
+
+	// constructor 
+	Player(int x, int y)  { 
+		x_pos = x; 
+		y_pos = y; 
+		width = 100;
+		height = 100;
+	} 
+	
+	public void moveX(int speed) { 
+		x_pos += speed; 
+	} 
+
+	public void moveY(int speed) {
+		y_pos += speed;
+	}
+
+	// draw the player 
+	public void drawPlayer(Graphics g) { 
+		  g.setColor(Color.blue);
+		  g.fillRect(x_pos, y_pos, width, height);
+	} 
+	
+} 
