65650210dc24e62df298cbc987f540b371bca039
Andrew Lee <andrewjplee586@gmail.com>
2016-07-23 12:45:21 -0400

    bullet class

---
+++ src/JebBushIsAMess/Bullet.java
@@ -0,0 +1,23 @@
+package JebBushIsAMess;
+
+import java.awt.Graphics; 
+import java.awt.Color; 
+
+public class Bullet {
+	private int x_pos;
+	private int y_pos;
+	
+	public Bullet(int x, int y) {
+		x_pos = x;
+		y_pos = y;
+	}
+	
+	public void moveShot(int speed) {
+		x_pos += speed;
+	}
+	
+	public void drawShot(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.drawOval(x_pos, y_pos,10, 10);
+	}
+}
